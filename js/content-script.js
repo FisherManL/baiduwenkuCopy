6b30475943d58ef464245dfab58da7d178fbc21e
hjliao <hjliao@qq.com>
2020-11-15 16:19:27 +0800

    增加更新多页文本按钮，优化显示

--- js/content-script.js
+++ js/content-script.js
@@ -1,14 +1,35 @@
-$(document).ready(function () {
-	console.log("插件运行");
+var Copy = function()
+{
+	// 开启复制
 	window.document.oncontextmenu = function(){        return true;  }  
 	$(".ie-fix").bind(oncontextmenu, function (e) {
 		return true;
 	});
-	var text = $(".ie-fix").text();
-	text = text.replace(/来自.*百度文库/i,"");
-	text = text.replace(/来自.*{display: none; font-size: 12px;}/i,"");
-	text = '<p style="font-size:18px;"><hr>'+text+'</p>'
-	$("#right-wrapper-id").html(text);
+	//取消复制屏蔽（vip内容）
+
 	$("#reader-container").addClass("reader-container");
 	$(".reader-container").removeAttr("id","reader-container");
+	//获取文本内容
+
+	var text = $(".ie-fix").text();
+	// 过滤字符
+	text = text.replace(/来自.*百度文库/i,"");
+	text = text.replace(/来自.*百度/i,"");
+	text = text.replace(/来自.*{display: none; font-size: 12px;}/i,"");
+	text = text.replace(/\n文库/i,"");
+	//展示
+	text = '<p style="font-size:15px; text-align:center;"><a href="https://github.com/JerwinLiao/baiduwenkuCopy">***Made By hjliao***</a><hr><button id="updateText" style="font-size:15px;display:block;margin:0 auto;">更新文本</button><hr>'+text+'</p>'
+	$("#right-wrapper-id").html(text);
+	console.log(text);
+}
+var SetButtonClick = function()
+{
+	$("#updateText").on("click",function(){
+		Copy();
+	});
+}
+$.when( $.ready ).then(function() {
+	console.log("插件运行");
+	Copy();
+	SetButtonClick();
 });
